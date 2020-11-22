eca1afd390a75be348a0aab4497b4779e4aed0f5
Arthur Deng <arthur.deng@ubtrobot.com>
2020-11-22 16:58:46 +0800

    newcanvas html working with socket.io, missing reset, redo, undo functions

--- public/js/newcanvas.js
+++ public/js/newcanvas.js
@@ -1,46 +1,81 @@
-function _(selector){
+function _(selector) {
     return document.querySelector(selector);
-  }
-  
-  function setup(){
+}
+
+function setup() {
     let canvas = createCanvas(650, 540);
     canvas.parent("canvas-wrapper");
     background(255);
-  }
-  function mouseDragged(){
-    let type = _("#pen-brush").checked?"brush":"eraser";
-    let size = parseInt(_("#pen-size").value);
-    
-    function changeColor(color){
-      fill(color);
-      stroke(color);    
+}
+
+function mouseDragged() {
+    type = _("#pen-brush").checked ? "brush" : "eraser";
+    size = parseInt(_("#pen-size").value);
+
+    function changeColor(color) {
+        fill(color);
+        stroke(color);
     }
-    
-    function drawing(){
-      strokeWeight(size);
-      line(pmouseX, pmouseY, mouseX, mouseY);    
+
+    function drawing() {
+        points.push({
+            px: pmouseX,
+            py: pmouseY,
+            x: mouseX,
+            y: mouseY,
+            brushSize: size,
+            brushColor: color,
+            brushType: type
+        });
     }
-    
-    if(type == "brush"){
-      let color = _("#pen-color").value;
-      changeColor(color);
-      drawing();
-    } 
-    else {
-      let color = 'rgb(255)';
-      changeColor(color);
-      drawing();
-    } 
-  }
-  _("#reset-canvas").addEventListener("click", function(){
-    background(255);
-  });
-  _("#undo").addEventListener("click", function(){
-    let context = drawingContext;
-    context.undo();
-  });
-  /*
-  seems like a good reference for p5.js undo redo functions
-  https://www.openprocessing.org/sketch/131411/
-  */
-  
+    if (type == "brush") {
+        color = _("#pen-color").value;
+    } else {
+        color = 'rgb(255)';
+    }
+    drawing();
+    updated = false;
+}
+
+_("#reset-canvas").addEventListener("click", function() {
+    points.push({
+        brushType: 'reset'
+    });
+});
+_("#undo").addEventListener("click", function() {
+    //ctx.undo();
+});
+
+// let ctx = drawingContext;
+let type = _("#pen-brush").checked ? "brush" : "eraser";
+let size = parseInt(_("#pen-size").value);
+let color = _("#pen-color").value;
+let updated = true;
+let points = [];
+let newStroke = true;
+let strokeCnt = 0;
+
+
+setInterval(function() {
+    if (updated) return;
+    socket.emit('draw', {
+        "point": points[points.length - 1]
+    });
+    updated = true;
+}, 10);
+
+socket.on('draw', (data) => {
+    let point = data.point;
+    if (point.brushType == 'reset') {
+        background(255);
+    } else {
+        fill(point.brushColor);
+        stroke(point.brushColor);
+        strokeWeight(point.brushSize);
+        line(point.px, point.py, point.x, point.y);
+    }
+});
+/*
+seems like a good reference for p5.js undo redo functions
+https://www.openprocessing.org/sketch/131411/
+*/
